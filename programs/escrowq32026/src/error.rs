b070a46c73b545a00063edee7ff1ad316977809d
HarmishTervadiya <harmishtervadiya@gmail.com>
2026-09-03 12:19:05 +0530

    feat: implement the take instruction
    - defined the necessary accounts and built the Take struct
    - created take function and added the necessary cpi calls to transfer the funds and close the vault
    - added a new error in custom ErrorCode

--- programs/escrowq32026/src/error.rs
+++ programs/escrowq32026/src/error.rs
@@ -4,4 +4,7 @@ use anchor_lang::prelude::*;
 pub enum ErrorCode {
     #[msg("The escrow has expired")]
     EscrowExpired,
+
+    #[msg("Insufficient funds")]
+    InsufficinetFunds
 }
--- programs/escrowq32026/src/instructions/take.rs
+++ programs/escrowq32026/src/instructions/take.rs
@@ -1,1 +1,120 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{
+        close_account, transfer_checked, CloseAccount, Mint, TokenAccount, TokenInterface,
+        TransferChecked,
+    },
+};
 
+use crate::{state::Escrow, ESCROW_SEED, ErrorCode};
+
+#[derive(Accounts)]
+pub struct Take<'info> {
+    #[account(mut)]
+    pub taker: Signer<'info>,
+
+    #[account(
+        init_if_needed,
+        payer= taker,
+    associated_token::authority = taker,
+    associated_token::mint = mint_a,
+    associated_token::token_program = token_program
+    )]
+    pub taker_ata_a: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_b,
+        associated_token::authority = taker
+    )]
+    pub taker_ata_b: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub maker: SystemAccount<'info>,
+
+    #[account(
+        init_if_needed,
+        payer= taker,
+        associated_token::mint = mint_b,
+        associated_token::authority = maker,
+        associated_token::token_program = token_program
+)]
+    pub maker_ata_b: InterfaceAccount<'info, TokenAccount>,
+
+    pub mint_a: InterfaceAccount<'info, Mint>,
+    pub mint_b: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mut,
+        close = maker,
+        has_one = mint_a,
+        has_one = mint_b,
+        has_one = maker,
+        seeds = [ESCROW_SEED, maker.key().as_ref(), escrow.seed.to_le_bytes().as_ref()],
+        bump = escrow.bump
+    )]
+    pub escrow: Account<'info, Escrow>,
+
+    #[account(mut,
+    associated_token::mint = mint_a,
+    associated_token::authority = escrow
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+impl<'info> Take<'info> {
+    pub fn take(&mut self) -> Result<()> {
+
+        // Prior check so we do not waste compute, 
+        // it will throw error due to name collision with anchor lang, 
+        // require!(self.taker_ata_b.amount >= self.escrow.receive, ErrorCode::InsufficinetFunds);
+
+        let signer_seeds: [&[&[u8]]; 1] = [&[
+            ESCROW_SEED,
+            self.maker.key.as_ref(),
+            &self.escrow.seed.to_le_bytes()[..],
+            &[self.escrow.bump],
+        ]];
+
+        let cpi_program = self.token_program.key();
+
+        let cpi_accounts = TransferChecked {
+            authority: self.escrow.to_account_info(),
+            from: self.vault.to_account_info(),
+            to: self.taker_ata_a.to_account_info(),
+            mint: self.mint_a.to_account_info(),
+        };
+
+        let cpi_context = CpiContext::new_with_signer(cpi_program, cpi_accounts, &signer_seeds);
+
+        transfer_checked(cpi_context, self.vault.amount, self.mint_a.decimals)?;
+
+        let cpi_accounts = TransferChecked {
+            authority: self.taker.to_account_info(),
+            from: self.taker_ata_b.to_account_info(),
+            to: self.maker_ata_b.to_account_info(),
+            mint: self.mint_b.to_account_info(),
+        };
+
+        let cpi_context = CpiContext::new(cpi_program, cpi_accounts);
+
+        transfer_checked(cpi_context, self.escrow.receive, self.mint_b.decimals)?;
+
+        let cpi_accounts = CloseAccount {
+            account: self.vault.to_account_info(),
+            destination: self.maker.to_account_info(),
+            authority: self.escrow.to_account_info(),
+        };
+
+        let cpi_context = CpiContext::new_with_signer(cpi_program, cpi_accounts, &signer_seeds);
+
+        close_account(cpi_context)?;
+
+        Ok(())
+    }
+}
